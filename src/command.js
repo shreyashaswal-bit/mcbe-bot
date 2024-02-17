f3a5b20474bd1864ff48c8261e9169add075a3b2
XiYang6666 <1782356858@qq.com>
2024-02-17 11:33:28 +0800

    :bug: fix disconnect error

--- src/command.js
+++ src/command.js
@@ -12,7 +12,7 @@ parser
     .command("exit")
     .description("退出程序")
     .action(async () => {
-        this.disconnect()
+        bot.close()
         process.exit(0)
     })
 
@@ -69,7 +69,7 @@ parser
 function parse(bot_, argv) {
     bot = bot_
     try {
-        parser.parse([null, null].concat(argv),)
+        parser.parse([null, null].concat(argv))
     }
     catch { }
 }
