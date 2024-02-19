b6367ec270d3d6c1d66433f539077f353790d252
XiYang6666 <1782356858@qq.com>
2024-02-19 15:15:24 +0800

    :label: update form.d.ts

--- src/mc/form.d.ts
+++ src/mc/form.d.ts
@@ -1,6 +1,22 @@
 import { Bot } from "../bot";
 
+type FormContent = {
+    type: "label" | "input" | "dropdown";
+    text: string;
+    options: string[] | undefined;
+};
+
+type FormButton = {
+    test: string;
+};
+
 export declare class Form {
+    bot: Bot;
+    id: number;
+    type: "form" | "custom_form";
+    title: string;
+    content: FormContent | string;
+    buttons: FormButton[];
     constructor(bot: Bot, param: object);
     render(): string;
     show(): void;
