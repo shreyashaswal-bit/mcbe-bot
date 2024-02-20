58337381ab7d59483c50442d2b219980787597ec
XiYang6666 <1782356858@qq.com>
2024-02-20 13:10:10 +0800

    :label: update context

--- src/loader/context.d.ts
+++ src/loader/context.d.ts
@@ -3,15 +3,17 @@ import { Context } from "../../type/context";
 import { Bot } from "../bot";
 import { Console } from "../console";
 import { Form } from "../mc/form";
-import { Player } from "../../type/player";
+import { Player } from "../mc/player";
 
 export class ContextImpl implements Context {
     bot: Bot;
     console: Console;
     parser: Command;
 
-    on(event_name: "player_list_update", callback: (player_list: Player[]) => void): Bot;
+    on(event_name: "player_join", callback: (player: Player) => void): Bot;
+    on(event_name: "player_leave", callback: (player: Player) => void): Bot;
+    on(event_name: "player_list_update", callback: (player_list: { [uuid: string]: Player }) => void): Bot;
     on(event_name: "message", callback: (message: string) => void): Bot;
     on(event_name: "form", callback: (form: Form) => void): Bot;
-    on(event_name: any, callback: (data: any) => void): Bot;
+    on(event_name: string, callback: (data: any) => void): Bot;
 }
--- type/context.d.ts
+++ type/context.d.ts
@@ -2,15 +2,17 @@ import { Command } from "commander";
 import { Bot } from "../src/bot";
 import { Console } from "../src/console";
 import { Form } from "../src/mc/form";
-import { Player } from "./player";
+import { Player } from "../src/mc/player";
 
 export declare type Context = {
     bot: Bot;
     console: Console;
     parser: Command;
 
-    on(event_name: "player_list_update", callback: (player_list: Player[]) => void): Bot;
+    on(event_name: "player_join", callback: (player: Player) => void): Bot;
+    on(event_name: "player_leave", callback: (player: Player) => void): Bot;
+    on(event_name: "player_list_update", callback: (player_list: { [uuid: string]: Player }) => void): Bot;
     on(event_name: "message", callback: (message: string) => void): Bot;
     on(event_name: "form", callback: (form: Form) => void): Bot;
-    on(event_name: any, callback: (data: any) => void): Bot;
+    on(event_name: string, callback: (data: any) => void): Bot;
 };
