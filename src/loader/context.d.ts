73ad528916f69673e8dcb118452fc8ecd2383296
XiYang6666 <1782356858@qq.com>
2024-03-02 00:35:51 +0800

    :label: update type

--- src/loader/context.d.ts
+++ src/loader/context.d.ts
@@ -4,7 +4,7 @@ import { Context } from "../../types/context";
 import { Config } from "../../types/config";
 import { Form } from "../mc/form";
 import { Player } from "../mc/player";
-import { Bot } from "../bot";
+import { Bot, ExCallback } from "../bot";
 import { Console } from "../console";
 
 export class ContextImpl implements Context {
@@ -15,17 +15,17 @@ export class ContextImpl implements Context {
 
     constructor(param: { bot: Bot; console: Console; parser: Command; config: Config });
 
-    on(event_name: "player_join", callback: (player: Player) => void): Bot;
-    on(event_name: "player_leave", callback: (player: Player) => void): Bot;
-    on(event_name: "player_list_update", callback: (player_list: { [uuid: string]: Player }) => void): Bot;
-    on(event_name: "message", callback: (message: string) => void): Bot;
-    on(event_name: "form", callback: (form: Form) => void): Bot;
-    on(event_name: string, callback: (data: any) => void): Bot;
+    on(event_name: "player_join", callback: ExCallback<Player>): Bot;
+    on(event_name: "player_leave", callback: ExCallback<Player>): Bot;
+    on(event_name: "player_list_update", callback: ExCallback<{ [uuid: string]: Player }>): Bot;
+    on(event_name: "message", callback: ExCallback<Text>): Bot;
+    on(event_name: "form", callback: ExCallback<Form>): Bot;
+    on(event_name: string, callback: (...param: any) => void): Bot;
 
-    once(event_name: "player_join", callback: (player: Player) => void): Bot;
-    once(event_name: "player_leave", callback: (player: Player) => void): Bot;
-    once(event_name: "player_list_update", callback: (player_list: { [uuid: string]: Player }) => void): Bot;
-    once(event_name: "message", callback: (message: string) => void): Bot;
-    once(event_name: "form", callback: (form: Form) => void): Bot;
-    once(event_name: string, callback: (data: any) => void): Bot;
+    once(event_name: "player_join", callback: ExCallback<Player>): Bot;
+    once(event_name: "player_leave", callback: ExCallback<Player>): Bot;
+    once(event_name: "player_list_update", callback: ExCallback<{ [uuid: string]: Player }>): Bot;
+    once(event_name: "message", callback: ExCallback<Text>): Bot;
+    once(event_name: "form", callback: ExCallback<Form>): Bot;
+    once(event_name: string, callback: (...param: any) => void): Bot;
 }
