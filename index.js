e3febb5d97d4d785fa16c65033f6d7866768a9fa
shreyashaswal-bit <shreyashaswal@gmail.com>
2026-03-11 18:04:13 +0530

    index.js

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-host: "Asnhuaswal.aternos.me",
-port: 56898,
-username: "AFKBot"
+const config = {
+  host: "Asnhuaswal.aternos.me",
+  port: 56898,
+  username: "AFKBot"
+}
+
+console.log("Connecting to server:", config.host)
