fbc855ac6158190a03a891c4416f7d616727ba0f
shreyashaswal-bit <shreyashaswal@gmail.com>
2026-03-13 10:15:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,55 +9,33 @@ const bot = bedrock.createClient({
   offline: true
 })
 
-let runtimeId = null
-
-bot.on("start_game", (packet) => {
-  runtimeId = packet.runtime_entity_id
-})
-
 bot.on("join", () => {
+
   console.log("USHA joined server")
 
-  // Jump every 5 seconds
+  setInterval(() => {
+    bot.queue("player_action", { action: "jump" })
+  }, 4000)
+
+  // keep alive
   setInterval(() => {
     try{
-      bot.queue("player_action", { action: "jump" })
-    }catch(e){}
-  }, 5000)
-
-  // Random look movement
-  setInterval(() => {
-
-    if(!runtimeId) return
-
-    try{
-      bot.queue("move_player", {
-        runtime_id: runtimeId,
-        position: {
-          x: Math.random()*2,
-          y: 0,
-          z: Math.random()*2
-        },
-        pitch: Math.random()*20,
-        yaw: Math.random()*360,
-        head_yaw: Math.random()*360,
-        mode: 3,
-        on_ground: true,
-        ridden_runtime_id: 0
+      bot.queue("tick_sync", {
+        request_time: Date.now(),
+        response_time: Date.now()
       })
     }catch(e){}
-
-  }, 7000)
+  }, 10000)
 
 })
 
 bot.on("disconnect", () => {
-  console.log("Disconnected... reconnecting in 10s")
-  setTimeout(startBot, 10000)
+  console.log("Disconnected... reconnecting")
+  setTimeout(startBot, 5000)
 })
 
 bot.on("error", err => {
-  console.log("Error:", err.message)
+  console.log(err)
 })
 
 }
